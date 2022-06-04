c23a72afd7a283cb31d2be5d8cbe4816a0b88463
Bipin Thapa <bt.kaji@gmail.com>
2022-06-04 21:22:09 +0545

    added regex for web url input

--- wpdown/pages/index.js
+++ wpdown/pages/index.js
@@ -37,6 +37,7 @@ export default function Home() {
                 type="text"
                 placeholder="Enter the name of the website you want to inspect..."
                 id="search-input"
+                pattern="(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"
               />
             </div>
             {/* Search Button */}
