e51465d4baad96b6578aa4e40cb664385ee3ae2a
Bipin Thapa <bt.kaji@gmail.com>
2022-06-07 15:55:23 +0545

    flowbite fix for build issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="../path/to/flowbite/dist/flowbite.js" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  plugins: [require("flowbite/plugin")],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
