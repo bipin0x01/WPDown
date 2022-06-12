6c7cd180f5488bc8f220de144b53b15f8b419daa
Bipin Thapa <bt.kaji@gmail.com>
2022-06-12 12:33:00 +0545

    fix:backend link

--- pages/index.js
+++ pages/index.js
@@ -1,178 +1,196 @@
-import Head from "next/head";
-import { useEffect, useState } from "react";
-import Loader from "../src/components/Loader";
-import { ToastContainer, toast } from "react-toastify";
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+import Loader from '../src/components/Loader'
+import { ToastContainer, toast } from 'react-toastify'
 
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css'
 
 // import fake data
-import fakeData from "../src/fakeData";
+import fakeData from '../src/fakeData'
 
 // Components Import
-import PluginSection from "../src/components/Main/PluginSection";
-import ThemeSection from "../src/components/Main/ThemeSection";
+import PluginSection from '../src/components/Main/PluginSection'
+import ThemeSection from '../src/components/Main/ThemeSection'
 
 // Services
-import { getWebsiteData } from "../src/services/getWebsiteData";
+import { getWebsiteData } from '../src/services/getWebsiteData'
 
 // Get url and convert it to https version
 const correctedUrl = (url) => {
-  if (url.includes("http://")) {
-    return url.replace("http://", "https://");
-  } else if (url.includes("https://")) {
-    return url;
-  } else {
-    return `https://${url}`;
-  }
-};
+    if (url.includes('http://')) {
+        return url.replace('http://', 'https://')
+    } else if (url.includes('https://')) {
+        return url
+    } else {
+        return `https://${url}`
+    }
+}
 
 export default function Home() {
-  const [url, setUrl] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [webData, setWebData] = useState(null);
+    const [url, setUrl] = useState('')
+    const [submitted, setSubmitted] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [webData, setWebData] = useState(null)
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    const Validurl = correctedUrl(url);
-    const data = await getWebsiteData(Validurl);
-    if (data) {
-      setWebData(data);
-      setLoading(false);
-      toast.success("Website Data Fetched Successfully");
-    } else {
-      setWebData(null);
-      setLoading(false);
-      toast.error(
-        "Sorry! The website you entered doesnot seem to be using wordpress."
-      );
-      // 2 sec delay
-      setTimeout(() => {
-        toast.info("Please check the URL and try again.");
-      }, 1500);
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setSubmitted(true)
+        console.log('form submitted with url: ', url)
     }
-  };
 
-  // Set the results to null for initial
-  useEffect(() => {
-    setWebData(null);
-  }, []);
+    // Set the results to null for initial
+    useEffect(() => {
+        setWebData(null)
+    }, [])
 
-  // get value from current state of url
-  return (
-    <div className="relative h-screen w-screen min-h-screen overflow-x-auto transition-duration-300 ease-in">
-      <Head>
-        <title>WPDown - Watcha Theme!</title>
-        <meta
-          name="description"
-          content="WPDown - Check for Exposed Theme Files"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {/* Header */}
-      <div className="main-top cointainer h-halfscreen ">
-        <ToastContainer
-          position="top-center"
-          autoClose={1000}
-          hideProgressBar={false}
-          newestOnTop={true}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-        <div className="flex flex-col justify-center h-full ">
-          <h1 className="font-bold text-center font-im-fell m-auto">
-            <span className="sm:text-8xl text-5xl">WPDOWN</span>
-            <br />
-            <span className="sm:text-4xl text-2xl">Watcha Theme!</span>
-          </h1>
-        </div>
-      </div>
-      {/* Header End */}
-      {/* Main Content */}
-      {/* Search Section */}
-      <div
-        className={`z-0 flex main absolute inset-0 justify-center h-full w-full my-auto`}
-      >
-        <form
-          onSubmit={handleSubmit}
-          className={`fixed text-white bg-inherit xl:w-halfscreen sm:w-3/4 rounded-xl sm:h-screen/4 p-4  place-self-center flex-row transition transition-duration-400 ease-in auto  shadow-slate-400 ${
-            loading &&
-            "bg-smooth-blue border-0 shadow-0 hover:shadow-0 bg-inherit "
-          }`}
-        >
-          {loading ? (
-            <Loader />
-          ) : (
-            <>
-              {/* Search Area */}
-              <div className="search-area py-2 px-4 rounded-xl bg-smooth-blue flex flex-row justify-center h-full items-center">
-                {/* Search Box with Icon */}
+    useEffect(() => {
+        if (submitted) {
+            setWebData(null)
+            setLoading(true)
+            const fetch = async () => {
+                const Validurl = correctedUrl(url)
+                const data = await getWebsiteData(Validurl)
+                if (data) {
+                    setWebData(data)
+                    setLoading(false)
+                    toast.success('Website Data Fetched Successfully')
+                } else {
+                    setWebData(null)
+                    setLoading(false)
+                    toast.error(
+                        'Sorry! The website you entered doesnot seem to be using wordpress.'
+                    )
+                    // 2 sec delay
+                    setTimeout(() => {
+                        toast.info('Please check the URL and try again.')
+                    }, 1500)
+                }
+            }
+            fetch()
+            setSubmitted(false)
+        }
+    }, [submitted])
 
-                <div
-                  className="search-box border-white border-2 m-2 p-2 rounded-2xl w-3/5 sm:w-4/5 shadow-lg hover:shadow-md transition duration-150 ease-in-out"
-                  id="search-box"
-                >
-                  {/* Text Input */}
-                  <div className="search-input ">
-                    <input
-                      className="search-input form-control w-full focus:outline-none px-2 bg-smooth-blue text-slate-300 font-bold"
-                      type="text"
-                      placeholder="https://"
-                      id="search-input"
-                      value={url}
-                      onChange={(e) => setUrl(e.target.value)}
-                      required
-                      autoComplete="off"
-                    />
-                  </div>
+    // get value from current state of url
+    return (
+        <div className="relative h-screen w-screen min-h-screen overflow-x-auto transition-duration-300 ease-in">
+            <Head>
+                <title>WPDown - Watcha Theme!</title>
+                <meta
+                    name="description"
+                    content="WPDown - Check for Exposed Theme Files"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {/* Header */}
+            <div className="main-top cointainer h-halfscreen ">
+                <ToastContainer
+                    position="top-center"
+                    autoClose={1000}
+                    hideProgressBar={false}
+                    newestOnTop={true}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
+                <div className="flex flex-col justify-center h-full ">
+                    <h1 className="font-bold text-center font-im-fell m-auto">
+                        <span className="sm:text-8xl text-5xl">WPDOWN</span>
+                        <br />
+                        <span className="sm:text-4xl text-2xl">
+                            Watcha Theme!
+                        </span>
+                    </h1>
                 </div>
-                {/* Search Button */}
-                <div className="search-button w-auto ">
-                  <button
-                    type="submit"
-                    className="search-btn text-white mx-3 px-4 py-2 font-bold rounded-3xl border-2 border-white flex flex-row leading-tight uppercase hover:text-smooth-blue hover:bg-white focus:outline-none focus:ring-0 transition duration-150 ease-in-out items-center"
-                  >
-                    <div>
-                      <span>Search</span>
-                    </div>
-                  </button>
-                </div>
-                {/* Search button End */}
-              </div>
-              {/* Search Area End*/}
-            </>
-          )}
-        </form>
-      </div>
-      {/* Search Section End */}
-
-      {/* Search Results Section */}
-      <div
-        className={`flex main-bottom bg-slate-200 text-white cointainer justify-center max-h-fit h-fit ${
-          webData ? "min-h-full" : ""
-        }`}
-      >
-        {webData && (
-          <>
-            <div className="z-40 flex flex-col container bg-white text-smooth-blue shadow-xl shadow-slate-300 items-center mt-40 p-10">
-              {/* Results Section */}
-              {/* Theme Section */}
-              <ThemeSection
-                themeDetails={webData.themeDetails}
-                themeDownload={webData.themeDownload}
-              />
-              {/* Plugin Container */}
-              <PluginSection plugins={webData.plugins} />
-              {/* Plugin Container End */}
             </div>
-          </>
-        )}
-      </div>
-      {/* Search Results Section End */}
-      {/* Main Content End */}
-    </div>
-  );
+            {/* Header End */}
+            {/* Main Content */}
+            {/* Search Section */}
+            <div
+                className={`z-0 flex main absolute inset-0 justify-center h-full w-full my-auto`}
+            >
+                <form
+                    onSubmit={handleSubmit}
+                    className={`fixed text-white bg-inherit xl:w-halfscreen sm:w-3/4 rounded-xl sm:h-screen/4 p-4  place-self-center flex-row transition transition-duration-400 ease-in auto  shadow-slate-400 ${
+                        loading &&
+                        'bg-smooth-blue border-0 shadow-0 hover:shadow-0 bg-inherit '
+                    }`}
+                >
+                    {loading ? (
+                        <Loader />
+                    ) : (
+                        <>
+                            {/* Search Area */}
+                            <div className="search-area py-2 px-4 rounded-xl bg-smooth-blue flex flex-row justify-center h-full items-center">
+                                {/* Search Box with Icon */}
+
+                                <div
+                                    className="search-box border-white border-2 m-2 p-2 rounded-2xl w-3/5 sm:w-4/5 shadow-lg hover:shadow-md transition duration-150 ease-in-out"
+                                    id="search-box"
+                                >
+                                    {/* Text Input */}
+                                    <div className="search-input ">
+                                        <input
+                                            className="search-input form-control w-full focus:outline-none px-2 bg-smooth-blue text-slate-300 font-bold"
+                                            type="text"
+                                            name="url"
+                                            placeholder="https://"
+                                            id="search-input"
+                                            value={url}
+                                            onChange={(e) =>
+                                                setUrl(e.target.value)
+                                            }
+                                            required
+                                            autoComplete="off"
+                                        />
+                                    </div>
+                                </div>
+                                {/* Search Button */}
+                                <div className="search-button w-auto ">
+                                    <button
+                                        type="submit"
+                                        className="search-btn text-white mx-3 px-4 py-2 font-bold rounded-3xl border-2 border-white flex flex-row leading-tight uppercase hover:text-smooth-blue hover:bg-white focus:outline-none focus:ring-0 transition duration-150 ease-in-out items-center"
+                                    >
+                                        <div>
+                                            <span>Search</span>
+                                        </div>
+                                    </button>
+                                </div>
+                                {/* Search button End */}
+                            </div>
+                            {/* Search Area End*/}
+                        </>
+                    )}
+                </form>
+            </div>
+            {/* Search Section End */}
+
+            {/* Search Results Section */}
+            <div
+                className={`flex main-bottom bg-slate-200 text-white cointainer justify-center max-h-fit h-fit ${
+                    webData ? 'min-h-full' : ''
+                }`}
+            >
+                {webData && (
+                    <>
+                        <div className="z-40 flex flex-col container bg-white text-smooth-blue shadow-xl shadow-slate-300 items-center mt-40 p-10">
+                            {/* Results Section */}
+                            {/* Theme Section */}
+                            <ThemeSection
+                                themeDetails={webData.themeDetails}
+                                themeDownload={webData.themeDownload}
+                            />
+                            {/* Plugin Container */}
+                            <PluginSection plugins={webData.plugins} />
+                            {/* Plugin Container End */}
+                        </div>
+                    </>
+                )}
+            </div>
+            {/* Search Results Section End */}
+            {/* Main Content End */}
+        </div>
+    )
 }
--- src/components/Main/ThemeSection.jsx
+++ src/components/Main/ThemeSection.jsx
@@ -1,115 +1,123 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import Button from "../Button";
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import Button from '../Button'
 
 export default function ThemeSection({ themeDetails, themeDownload }) {
-  return (
-    <div className="theme-section w-full sm:p-0 p-4 border-2 shadow-lg hover:shadow-md transition ease-in-out auto shadow-slate-200 m-2 my-4 ">
-      {/* Theme Section Left */}
+    return (
+        <div className="theme-section w-full sm:p-0 p-4 border-2 shadow-lg hover:shadow-md transition ease-in-out auto shadow-slate-200 m-2 my-4 ">
+            {/* Theme Section Left */}
 
-      <div className=" results-title m-2 p-2 px-4 text-white font-left bg-smooth-blue font-bold text-xl ">
-        Theme Details
-      </div>
-      {themeDetails.theme ? (
-        <section className=" flex flex-col md:flex-row items-center gap-2">
-          <div className="theme-section-left flex flex-row justify-center">
-            <div className="theme-screenshot w-full p-2">
-              <Image
-                src={
-                  themeDetails.screenshot
-                    ? `https://res.cloudinary.com/kaji/image/fetch/${themeDetails.screenshot}`
-                    : "https://res.cloudinary.com/kaji/image/fetch/https://via.placeholder.com/350*500"
-                }
-                alt="theme screenshot"
-                height={350}
-                width={500}
-                responsive={true}
-              />
+            <div className=" results-title m-2 p-2 px-4 text-white font-left bg-smooth-blue font-bold text-xl ">
+                Theme Details
             </div>
-          </div>
-          {/* Theme Section Left End */}
-          {/* Theme Section Right */}
-
-          <div className="theme-section-right w-full p-2">
-            <h3 className="m-4 text-left text-lg font-bold ">
-              <div className="theme-name mb-3">
-                Theme Name :{" "}
-                <span className="font-normal text-black ml-2">
-                  {themeDetails?.theme}
-                </span>
-              </div>
-              <div className="theme-version mb-3">
-                version:{" "}
-                <span className="font-normal text-black ml-2">
-                  {themeDetails?.version}
-                </span>
-              </div>
-              {themeDetails?.author && (
-                <div className="theme-author mb-3">
-                  Author:{" "}
-                  <span className="font-normal text-black ml-2">
-                    <Link href={themeDetails?.authorLink}>
-                      <a className="hover:text-smooth-blue">
-                        {themeDetails?.author}
-                      </a>
-                    </Link>
-                  </span>
-                </div>
-              )}
-              <div className="theme-tags whitespace-normal">
-                <div className="tags-group flex flex-row">
-                  <div className="tag-title ">Tags: </div>
-                  <div className="tags-list ml-2 flex flex-wrap font-medium">
-                    {themeDetails?.tags?.map((tag, index) => (
-                      <div
-                        className="text-sm m-2 hover:border-2 hover:bg-smooth-blue hover:text-white tag mx-1 mt-1 px-1 rounded-xl min-w-fit border-2 border-smooth-blue"
-                        key={index}
-                      >
-                        <Link href="#">
-                          <a className="p-1 my-1">{tag}</a>
-                        </Link>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              {/* Download Button */}
-              {themeDownload?.status === "success" ? (
-                // Download Button
-                <>
-                  <div className="download-button flex flex-row p-2 pl-0">
-                    <Button label="Download" link={themeDownload?.url} />
-                  </div>
-                </>
-              ) : (
-                <>
-                  <div className="download-button flex flex-row mt-6">
-                    <div className="text-md text-red-500">
-                      Exposed Theme File Not Found! Download Unavailable.
+            {themeDetails.theme ? (
+                <section className=" flex flex-col md:flex-row items-center gap-2">
+                    <div className="theme-section-left flex flex-row justify-center">
+                        <div className="theme-screenshot w-full p-2">
+                            <Image
+                                src={
+                                    themeDetails.screenshot
+                                        ? `https://res.cloudinary.com/kaji/image/fetch/${themeDetails.screenshot}`
+                                        : 'https://res.cloudinary.com/kaji/image/fetch/https://via.placeholder.com/350*500'
+                                }
+                                alt="theme screenshot"
+                                height={350}
+                                width={500}
+                                responsive="true"
+                            />
+                        </div>
                     </div>
-                  </div>
+                    {/* Theme Section Left End */}
+                    {/* Theme Section Right */}
+
+                    <div className="theme-section-right w-full p-2">
+                        <h3 className="m-4 text-left text-lg font-bold ">
+                            <div className="theme-name mb-3">
+                                Theme Name :{' '}
+                                <span className="font-normal text-black ml-2">
+                                    {themeDetails?.theme}
+                                </span>
+                            </div>
+                            <div className="theme-version mb-3">
+                                version:{' '}
+                                <span className="font-normal text-black ml-2">
+                                    {themeDetails?.version}
+                                </span>
+                            </div>
+                            {themeDetails?.author && (
+                                <div className="theme-author mb-3">
+                                    Author:{' '}
+                                    <span className="font-normal text-black ml-2">
+                                        <Link href={themeDetails?.authorLink}>
+                                            <a className="hover:text-smooth-blue">
+                                                {themeDetails?.author}
+                                            </a>
+                                        </Link>
+                                    </span>
+                                </div>
+                            )}
+                            <div className="theme-tags whitespace-normal">
+                                <div className="tags-group flex flex-row">
+                                    <div className="tag-title ">Tags: </div>
+                                    <div className="tags-list ml-2 flex flex-wrap font-medium">
+                                        {themeDetails?.tags?.map(
+                                            (tag, index) => (
+                                                <div
+                                                    className="text-sm m-2 hover:border-2 hover:bg-smooth-blue hover:text-white tag mx-1 mt-1 px-1 rounded-xl min-w-fit border-2 border-smooth-blue"
+                                                    key={index}
+                                                >
+                                                    <Link href="#">
+                                                        <a className="p-1 my-1">
+                                                            {tag}
+                                                        </a>
+                                                    </Link>
+                                                </div>
+                                            )
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                            {/* Download Button */}
+                            {themeDownload?.status === 'success' ? (
+                                // Download Button
+                                <>
+                                    <div className="download-button flex flex-row p-2 pl-0">
+                                        <Button
+                                            label="Download"
+                                            link={themeDownload?.url}
+                                        />
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <div className="download-button flex flex-row mt-6">
+                                        <div className="text-md text-red-500">
+                                            Exposed Theme File Not Found!
+                                            Download Unavailable.
+                                        </div>
+                                    </div>
+                                </>
+                            )}
+                        </h3>
+                    </div>
+                    {/* Theme Section Right End */}
+                </section>
+            ) : (
+                <>
+                    <div className="theme-section-left flex flex-row justify-center  m-4">
+                        {/* Theme Data Not Available */}
+                        <div className="theme-info w-full p-2">
+                            <div className="theme-name mb-3 text-lg text-center">
+                                <h2>
+                                    Sorry! We could not find any theme data for
+                                    this theme. Please try again later.
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
                 </>
-              )}
-            </h3>
-          </div>
-          {/* Theme Section Right End */}
-        </section>
-      ) : (
-        <>
-          <div className="theme-section-left flex flex-row justify-center  m-4">
-            {/* Theme Data Not Available */}
-            <div className="theme-info w-full p-2">
-              <div className="theme-name mb-3 text-lg text-center">
-                <h2>
-                  Sorry! We could not find any theme data for this theme. Please
-                  try again later.
-                </h2>
-              </div>
-            </div>
-          </div>
-        </>
-      )}
-    </div>
-  );
+            )}
+        </div>
+    )
 }
